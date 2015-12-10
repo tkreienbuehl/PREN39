a292311c41bbcecd08c55d76bcf53d9ddb5839f8
patbrant <pat.brantschen@gmail.com>
2015-12-10 10:02:21 +0100

    ObjectDetection in Projekt eingefügt

--- Sourcecode/WorkspacePren/GueselStar/src/source/GueselStar.cpp
+++ Sourcecode/WorkspacePren/GueselStar/src/source/GueselStar.cpp
@@ -6,12 +6,16 @@ int main() {
 
 	PictureCreator* PicCreator = new PictureCreator(controller);
 	RouteFinder* RtFinder = new RouteFinder(controller);
+	ObjectFinder* objectFinder = new ObjectFinder(controller);
+
 	RtFinder->FindRoute(PicCreator);
+	objectFinder->findObjects(PicCreator);
 
 	controller->Start();
 
 	PicCreator->StopRecording();
 	delete RtFinder;
+	delete objectFinder;
 	delete PicCreator;
 	delete controller;
 	return 0;
