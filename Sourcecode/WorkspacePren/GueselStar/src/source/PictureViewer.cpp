178a1308a4c3705f1ce63e0fe8d812f870b207e3
tobias <tkreienbuehl@bluewin.ch>
2015-12-11 15:23:10 +0100

    Daten vergessen

---
+++ Sourcecode/WorkspacePren/GueselStar/src/source/PictureViewer.cpp
@@ -0,0 +1,43 @@
+#include "../header/PictureViewer.hpp"
+
+	PictureViewer::PictureViewer(RouteFinder* rtFinder, ObjectFinder* objFinder) {
+		m_rtFinder = rtFinder;
+		m_objectFinder = objFinder;
+		m_state = false;
+	}
+
+	PictureViewer::~PictureViewer() {
+
+	}
+
+	void* PictureViewer::startThread(void* threadId) {
+		((PictureViewer*) threadId)->runProcess();
+		return threadId;
+	}
+
+	void PictureViewer::stopViewer() {
+		m_state = false;
+	}
+
+	void PictureViewer::runProcess() {
+
+		cv::Mat fltImage;
+
+		usleep(2000000);
+		m_state = true;
+
+		cout << "Viever online" << endl;
+		cv::namedWindow( "The Image", CV_WINDOW_AUTOSIZE );
+		while (m_state) {
+
+			fltImage = *m_rtFinder->getFilteredImage();
+
+			if (!fltImage.empty()) {
+				cv::imshow("The Image", fltImage);
+				cv::imshow("The Source gray Image", *m_rtFinder->getGrayImage());
+				cv::imshow("Object Detection", m_objectFinder->getImgae());
+				cv::waitKey(0);
+			}
+		}
+
+	}
