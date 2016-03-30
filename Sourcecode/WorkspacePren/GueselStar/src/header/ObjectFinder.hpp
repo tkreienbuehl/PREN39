2ad4ca676b569121d04fdb2b2802f83250b82715
patbrant <pat.brantschen@gmail.com>
2016-03-30 17:52:55 +0200

    Übergabeparameter und Rückgabewerte in Pointer umgeändert

--- Sourcecode/WorkspacePren/GueselStar/src/header/ObjectFinder.hpp
+++ Sourcecode/WorkspacePren/GueselStar/src/header/ObjectFinder.hpp
@@ -18,12 +18,12 @@ public:
 	~ObjectFinder();
 
 	void findObjects(PictureCreator* picCreator);
-	cv::Mat createImage(cv::String filename);
-	cv::Mat convertImageToHSV(cv::Mat rgbImage);
-	cv::Mat filterColorInImage(cv::String color, cv::Mat imageToFilter);
-	vector<vector<cv::Point> > findContainersInImage(cv::Mat imageToFindContainer);
-	cv::Mat markFindContoursInImage(vector<vector<cv::Point> > contours, cv::Mat imageToMarkContainer);
-	vector<vector<cv::Point> > mergeContours(vector<vector<cv::Point> > contours1, vector<vector<cv::Point> > contours2);
+	cv::Mat* createImage(cv::String filename);
+	cv::Mat* convertImageToHSV(cv::Mat* rgbImage);
+	cv::Mat* filterColorInImage(cv::String color, cv::Mat* imageToFilter);
+	vector<vector<cv::Point> >* findContainersInImage(cv::Mat* imageToFindContainer);
+	cv::Mat* markFindContoursInImage(vector<vector<cv::Point> >* contours, cv::Mat* imageToMarkContainer);
+	vector<vector<cv::Point> >* mergeContours(vector<vector<cv::Point> >* contours1, vector<vector<cv::Point> >* contours2);
 
 	static void* staticEntryPoint(void* threadId);
 
@@ -32,10 +32,10 @@ public:
 
 private:
 	void RunProcess();
-	vector<vector<cv::Point> > contoursGreen;
-	vector<vector<cv::Point> > contoursBlue;
-	vector<vector<cv::Point> > contours;
-	cv::Mat m_MarkedImage;
+	vector<vector<cv::Point> >* contoursGreen;
+	vector<vector<cv::Point> >* contoursBlue;
+	vector<vector<cv::Point> >* contours;
+	cv::Mat* m_MarkedImage;
 	PictureCreator* m_PicCreator;
 	PrenController* m_Controller;
 	bool m_state;
--- Sourcecode/WorkspacePren/GueselStar/src/source/ObjectFinder.cpp
+++ Sourcecode/WorkspacePren/GueselStar/src/source/ObjectFinder.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-ObjectFinder::ObjectFinder(PrenController* controller ,PictureCreator* picCreator) {
+ObjectFinder::ObjectFinder(PrenController* controller, PictureCreator* picCreator) {
 	m_Controller = controller;
 	m_PicCreator = picCreator;
 	m_state = false;
@@ -22,11 +22,11 @@ void* ObjectFinder::staticEntryPoint(void* threadId) {
 
 void ObjectFinder::RunProcess() {
 
-	cv::Mat origImage;
-	cv::Mat croppedImage;
-	cv::Mat hsvImage;
-	cv::Mat filteredImageGreen;
-	cv::Mat filteredImageBlue;
+	cv::Mat* origImage;
+	cv::Mat* croppedImage;
+	cv::Mat* hsvImage;
+	cv::Mat* filteredImageGreen;
+	cv::Mat* filteredImageBlue;
 
 	m_state = true;
 
@@ -36,8 +36,8 @@ void ObjectFinder::RunProcess() {
 
 		origImage = createImage("/home/patbrant/Pictures/pren/street_distance1.jpg");
 
-		if ( !origImage.empty()) {
-			cv::Rect areaToCrop(origImage.cols / 2, 0, origImage.cols / 2, origImage.rows);
+		if ( !(*origImage).empty()) {
+			cv::Rect areaToCrop((*origImage).cols / 2, 0, (*origImage).cols / 2, (*origImage).rows);
 			croppedImage = origImage(areaToCrop);
 			hsvImage = convertImageToHSV(croppedImage);
 			filteredImageGreen = filterColorInImage("green", hsvImage);
@@ -52,78 +52,78 @@ void ObjectFinder::RunProcess() {
 	}
 }
 
-cv::Mat ObjectFinder::createImage(cv::String filename) {
+cv::Mat* ObjectFinder::createImage(cv::String filename) {
 	cv::Mat srcImage = imread(filename, 1);
 
 	if (srcImage.data == NULL) {
 		printf("file cannot be loaded\n");
 	}
 
-	cv::Mat origImage;
+	cv::Mat* origImage;
 	cv::Size size(900, 675);
-	resize(srcImage, origImage, size);
+	resize(srcImage, *origImage, size);
 
 	return origImage;
 }
 
-cv::Mat ObjectFinder::convertImageToHSV(cv::Mat rgbImage) {
-	cv::Mat HSVImage;
+cv::Mat* ObjectFinder::convertImageToHSV(cv::Mat* rgbImage) {
+	cv::Mat* HSVImage;
 
-	cvtColor(rgbImage, HSVImage, CV_RGB2HSV);
+	cvtColor(*rgbImage, *HSVImage, CV_RGB2HSV);
 	return HSVImage;
-	}
+}
 
-	cv::Mat ObjectFinder::filterColorInImage(cv::String color,
-		cv::Mat imageToFilter) {
+cv::Mat* ObjectFinder::filterColorInImage(cv::String color,
+		cv::Mat* imageToFilter) {
 
 	cv::Mat filteredImage;
 	if (color == "green") {
-		cv::inRange(imageToFilter, cv::Scalar(38, 65, 10), cv::Scalar(75, 255, 255),
+		cv::inRange(*imageToFilter, cv::Scalar(38, 65, 10), cv::Scalar(75, 255, 255),
 				filteredImage);
 	} else if (color == "blue") {
-		cv::inRange(imageToFilter, cv::Scalar(10, 140, 90),
+		cv::inRange(*imageToFilter, cv::Scalar(10, 140, 90),
 				cv::Scalar(20, 255, 255), filteredImage);
 	} else if (color == "yellow") {
-		cv::inRange(imageToFilter, cv::Scalar(20, 100, 100),
+		cv::inRange(*imageToFilter, cv::Scalar(20, 100, 100),
 				cv::Scalar(38, 255, 255), filteredImage);
 	}
 
 	return filteredImage;
 }
 
-vector<vector<cv::Point> > ObjectFinder::findContainersInImage(
-	cv::Mat imageToFindContainer) {
+vector<vector<cv::Point> >* ObjectFinder::findContainersInImage(
+	cv::Mat* imageToFindContainer) {
 	vector<vector<cv::Point> > contours;
 	vector<cv::Vec4i> hierarchy;
-	cv::findContours(imageToFindContainer, contours, hierarchy, CV_RETR_TREE,
+	cv::findContours(*imageToFindContainer, contours, hierarchy, CV_RETR_TREE,
 			CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
 	return contours;
 }
 
-vector<vector<cv::Point> > ObjectFinder::mergeContours(
-	vector<vector<cv::Point> > contours1,
-	vector<vector<cv::Point> > contours2) {
-	vector<vector<cv::Point> > contours;
+vector<vector<cv::Point> >* ObjectFinder::mergeContours(
+	vector<vector<cv::Point>>* contours1,
+	vector<vector<cv::Point>>* contours2) {
+	vector<vector<cv::Point>>* contours;
 
-	contours.reserve(contours1.size() + contours2.size());
-	contours.insert(contours.end(), contours1.begin(), contours1.end());
-	contours.insert(contours.end(), contours2.begin(), contours2.end());
+	(*contours).reserve((*contours1).size() + (*contours2).size());
+	(*contours).insert((*contours).end(), (*contours1).begin(), (*contours1).end());
+	(*contours).insert((*contours).end(), (*contours2).begin(), (*contours2).end());
 
 	return contours;
 }
 
-cv::Mat ObjectFinder::markFindContoursInImage(
-	vector<vector<cv::Point> > contours, cv::Mat imageToMarkContainer) {
+cv::Mat* ObjectFinder::markFindContoursInImage(
+	vector<vector<cv::Point> >* contours, cv::Mat* imageToMarkContainer) {
 
-	vector<vector<cv::Point> > contours_poly(contours.size());
-	vector<cv::Rect> boundRect(contours.size());
-	cv::Mat markedImage = imageToMarkContainer;
-	for (unsigned int i = 0; i < contours.size(); i++) {
-		cv::approxPolyDP(cv::Mat(contours[i]), contours_poly[i], 3, true);
+	vector<vector<cv::Point> > contours_poly((*contours).size());
+	vector<cv::Rect> boundRect((*contours).size());
+	cv::Mat* markedImage = imageToMarkContainer;
+	for (unsigned int i = 0; i < (*contours).size(); i++) {
+		cv::approxPolyDP(cv::Mat((*contours)[i]), contours_poly[i], 3, true);
 		boundRect[i] = cv::boundingRect(cv::Mat(contours_poly[i]));
 
 		if (boundRect[i].height > boundRect[i].width && boundRect[i].height > 150) {
-			rectangle(markedImage, boundRect[i].tl(), boundRect[i].br(),
+			rectangle(*markedImage, boundRect[i].tl(), boundRect[i].br(),
 					cv::Scalar(0, 255, 0), 2, 8, 0);
 
 			int distanceToContainer = 280 * 216 / boundRect[i].height;
@@ -132,7 +132,7 @@ cv::Mat ObjectFinder::markFindContoursInImage(
 			 stringConverter << distanceToContainer;
 			 std::string distanceString = stringConverter.str();
 
-			putText(markedImage, "Distance to Container: " + distanceString + "mm", cvPoint(30,30),
+			putText(*markedImage, "Distance to Container: " + distanceString + "mm", cvPoint(30,30),
 					CV_FONT_HERSHEY_PLAIN, 1.5, cvScalar(200,200,250), 1, CV_AA);
 		}
 	}
