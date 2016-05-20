fdadf0267c071c2de10379e61c4cb08788998099
patbrant <pat.brantschen@gmail.com>
2016-05-20 08:59:33 +0200

    Programm auf FreedomBoard beenden, falls lane verloren wird

--- Sourcecode/WorkspacePren/GueselStar/src/source/PrenController.cpp
+++ Sourcecode/WorkspacePren/GueselStar/src/source/PrenController.cpp
@@ -99,6 +99,7 @@ void PrenController::setTargetFieldFound(int distance) {
 void PrenController::setLaneLost() {
 	// inform MC-Board
 	printString("RouterFinder lost Lane", CONTROLLER, 1);
+	uartSender->sendStopCmd();
 }
 
 void PrenController::setSteeringAngle(int angle) {
